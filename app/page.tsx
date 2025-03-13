6666ff292cc7818620d9ac8dbeb41b736f5e3ab6
Anunay Argha <anunayargha@gmail.com>
2025-03-13 19:43:27 +0600

    Change the module import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Hero from "@/components/hero";
+import Hero from "../components/hero";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
-
 			<div className="h-screen">Hello</div>
 		</>
 	);
